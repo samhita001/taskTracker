ae702027f721e49bbdcb6c493ac325c69fd3cd8e
samhita001 <samhitanew01@gmail.com>
2024-11-22 12:00:53 +0530

    updated

--- task-tracker/src/components/Task.js
+++ task-tracker/src/components/Task.js
@@ -2,23 +2,6 @@ import React from "react";
 import axios from "axios";
 
 const Task = ({ task, fetchTasks }) => {
-  const handleComplete = async () => {
-    const token = localStorage.getItem("token");
-    await axios.put(
-      `http://localhost:3000/api/tasks/${task._id}`,
-      {},
-      { headers: { Authorization: token } }
-    );
-    fetchTasks();
-  };
-
-  const handleDelete = async () => {
-    const token = localStorage.getItem("token");
-    await axios.delete(`http://localhost:3000/api/tasks/${task._id}`, {
-      headers: { Authorization: token },
-    });
-    fetchTasks();
-  };
 
   return (
     <div>
